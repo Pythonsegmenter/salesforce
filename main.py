5a95ad1017de8f3c2a8d55cd7834a378f4a2d78b
aspyro-coder <jeroen.vancraen@aspyro.be>
2022-04-03 00:09:58 +0200

    Now able to detail relationships and get statistics.

--- main.py
+++ main.py
@@ -40,79 +40,57 @@ sf = Salesforce(instance=instance, session_id=session_id)
 #Get datafram with all object api names that are customizable (=present in obj manager)
 obj_api_name_and_label = pd.DataFrame(sf.query("SELECT QualifiedApiName, Label FROM EntityDefinition WHERE IsCustomizable=True")['records'])
 
+#Initialize stat list
+stat_list = [] #counts number of links per obj
 
-
-#Set target label
-target_label = "Account"
-
-#Initialize link list & stat list
-stat_list = [] #counts number of links
-link_list = [] #details links
-
-## Check in the target object to what other objects it references
-#Get dataframe with field api names, labels & datatypes
-field_info = pd.DataFrame(sf.query(
-    "SELECT QualifiedApiName, Label, DataType, Description FROM FieldDefinition WHERE EntityDefinition.Label IN ('"+ target_label +"')")[
-                            'records'])
-for i,field_type in enumerate(field_info.loc[:,"DataType"]):
-    link, link_type, obj_label = check_target_obj_links(field_type)
-    if link:
-        link_list.append([target_label, obj_label, link_type, field_info.loc[i,"Label"], field_info.loc[i,"Description"]])
+stop_index =0
+stop_limit = 5 #Used for testing.
 
 ## Check for other objects with references to the target object
 #Iterate over the api names of all objects
-for label in obj_api_name_and_label.loc[:, "Label"]: #Iterate over the api_names (string types)
-    # print(label)
-    if label == target_label: #We already checked the target object.
-        continue
+for target_label in obj_api_name_and_label.loc[:, "Label"]: #Iterate over the api_names (string types)
 
+    #For testing purposes
+    if stop_limit == stop_index:
+        break
+    stop_index += 1
+
+    #Initialize link list
+    link_list = [] #details links
+
+    ## Check in the target object to what other objects it references
     #Get dataframe with field api names, labels & datatypes
     field_info = pd.DataFrame(sf.query(
-        "SELECT QualifiedApiName, Label, DataType FROM FieldDefinition WHERE EntityDefinition.Label IN ('"+ label +"')")[
+        "SELECT QualifiedApiName, Label, DataType, Description FROM FieldDefinition WHERE EntityDefinition.Label IN ('"+ target_label +"')")[
                                 'records'])
     for i,field_type in enumerate(field_info.loc[:,"DataType"]):
-        # print(field_type)
-        link, link_type, target_obj_match = check_link_non_target_obj(field_type, target_label)
-        if (link and target_obj_match):
-            link_list.append([label, target_label, link_type, field_info.loc[i,"Label"], field_info.loc[i,"Description"]]) #Link list item: [source obj, target obj, link type, field name]
+        link, link_type, obj_label = check_target_obj_links(field_type)
+        if link:
+            link_list.append([target_label, obj_label, link_type, field_info.loc[i,"Label"], field_info.loc[i,"Description"]])
+    links_to_other_objects = len(link_list)
 
-output = pd.DataFrame(link_list, columns=['Source obj', 'Target obj', "Link type", "Field name", "Field description"])
-stat_list.append([target_label,output.shape[0]])
-output.to_csv('/output/'+target_label+'.csv',index=False)
-print("a")
-        # print(field_info.loc[i,:])
+    ## Check for other objects with references to the target object
+    #Iterate over the api names of all objects
+    for label in obj_api_name_and_label.loc[:, "Label"]: #Iterate over the api_names (string types)
+        # print(label)
+        if label == target_label: #We already checked the target object.
+            continue
 
+        #Get dataframe with field api names, labels & datatypes
+        field_info = pd.DataFrame(sf.query(
+            "SELECT QualifiedApiName, Label, DataType, Description FROM FieldDefinition WHERE EntityDefinition.Label IN ('"+ label +"')")[
+                                    'records'])
+        for i,field_type in enumerate(field_info.loc[:,"DataType"]):
+            # print(field_type)
+            link, link_type, target_obj_match = check_link_non_target_obj(field_type, target_label)
+            if (link and target_obj_match):
+                link_list.append([label, target_label, link_type, field_info.loc[i,"Label"], field_info.loc[i,"Description"]]) #Link list item: [source obj, target obj, link type, field name]
 
-#df_table.to_csv('entity definitions.csv',index=False)
+    output = pd.DataFrame(link_list, columns=['Source obj', 'Target obj', "Link type", "Field name", "Field description"])
+    links_from_other_objects = len(link_list) - links_to_other_objects
+    stat_list.append([target_label,output.shape[0], links_to_other_objects, links_from_other_objects]) #add the statistics
+    output.to_csv('output/'+target_label+'.csv',index=False) #save the file
 
-#You have to say EntityDefintion.QualifiedApiName as if you would just say QualifiedApiName it would look at the FieldDefinition api name.
-df_field = pd.DataFrame(sf.query("SELECT EntityDefinition.QualifiedApiName, QualifiedApiName, DataType FROM FieldDefinition WHERE EntityDefinition.QualifiedApiName IN ('Account')")['records'])
-
-
-
-
-
-print("a")
-# df_field['TableName'] = df_field['EntityDefinition'].map(op.itemgetter('QualifiedApiName'))
-# df_field[['TableName', 'QualifiedApiName', 'DataType']].head()
-
-# metadata_org = sf.describe()
-# # print(metadata_org['encoding'])
-# # print(metadata_org['maxBatchSize'])
-# # print(metadata_org['sobjects'])
-# df_sobjects = pd.DataFrame(metadata_org['sobjects'])
-# df_sobjects.to_csv('org metadata info.csv', index=False)
-
-
-# method 1
-# project__c = sf.Project__c
-# metadata_project = project__c.metadata()
-# df_project_metadata = pd.DataFrame(metadata_project.get('objectDescribe'))
-# df_project_metadata.to_csv('project metadata.csv', index=False)
-
-
-# method 2
-# account = SFType('account', session_id, instance)
-# account_metadata = account.metadata()
-# df_account_metadata = pd.DataFrame(account_metadata.get('objectDescribe'))
-# df_account_metadata.to_csv('account metadata.csv', index=False)
+stats = pd.DataFrame(stat_list, columns=['obj', 'number of links', 'number of outgoing links', 'number of incoming links'])
+stats.to_csv('output/aaa_statistics.csv',index=False)
+print("Done")
